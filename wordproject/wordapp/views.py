381d9ed5149e8759d40c8981f9580a0e68e9d39f
chuchu387 <shubhamoverpowered@gmail.com>
2023-05-17 05:04:15 +0545

    Simple Word Counter

---
+++ wordproject/wordapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def counter(request):
+    text = request.GET['text']
+    words_counter = len(text.split())
+    return render(request, 'counter.html', {'total_words': words_counter})
